9801ca0f0e2e19dbad97b4f23506b6fe1784c2ad
Kevin Selm <kcselm93@gmail.com>
2023-12-06 16:10:44 -0800

    Setting up form state with name input

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formValues, setFormValues] = useState({
+    id: 1,
+    name: "",
+    location: "",
+  });
+  const [records, setRecords] = useState([]);
+
+  const handleNameChange = (e) => {
+    setFormValues({ ...formValues, name: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setRecords([...records, formValues]);
+    setFormValues({ id: formValues.id + 1, name: "", location: "" });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          value={formValues.name}
+          onChange={handleNameChange}
+        ></input>
+      </form>
     </div>
   );
 }
